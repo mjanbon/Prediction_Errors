8461acb2944810bd1019980f92f430442d70d9a5
Juho Äijälä <57116597+aijala@users.noreply.github.com>
2023-03-10 15:59:20 +0200

    Streamlining main
    own function for plotting figures instead of having it in the main - deleted plotting in main

--- Plotting/Plot_main.m
+++ Plotting/Plot_main.m
@@ -1,36 +1,6 @@
 %% PLOTS COI/MI WITH PERMUTATION TESTING FOR SPECIFIED PARTICIPANT AND CONDITION
 %...in the way we want to have it in the papers 
 
-%Plot JI_XY
-%Either within or between CoI
-
-%% GENERAL
-
-% set the tickdirs to go out - need this specific order
-set(groot, 'DefaultAxesTickDir', 'out');
-set(groot, 'DefaultAxesTickDirMode', 'manual');
-
-% general graphics, this will apply to any figure you open (groot is the default figure object).
-% I have this in my startup.m file, so I don't have to retype these things whenever plotting a new fig.
-set(groot, ...
-    'DefaultFigureColorMap', linspecer, ...
-    'DefaultFigureColor', 'w', ...
-    'DefaultAxesLineWidth', 0.5, ...
-    'DefaultAxesXColor', 'k', ...
-    'DefaultAxesYColor', 'k', ...
-    'DefaultAxesFontUnits', 'points', ...
-    'DefaultAxesFontSize', 8, ...
-    'DefaultAxesFontName', 'Helvetica', ...
-    'DefaultLineLineWidth', 1, ...
-    'DefaultTextFontUnits', 'Points', ...
-    'DefaultTextFontSize', 8, ...
-    'DefaultTextFontName', 'Helvetica', ...
-    'DefaultAxesBox', 'off', ...
-    'DefaultAxesTickLength', [0.02 0.025]);
-
-% type cbrewer without input args to see all possible sets of colormaps
-colors = cbrewer2('qual', 'Set1', 8);
-
 %% GETS ALL THE PARAMETERS FOR PARTICIPANT AND DATA
 
 %PLOTTING PARAMETERS!
@@ -60,77 +30,4 @@ iEEG1.times (end_cut_off:end) = [];
 %Get data in correct format
 [tempFFiall, tempFFm, tempFFmr, tempFFms, tempFFmi1, tempFFmi2, tempFFir, tempFFis, temp_nb, front_nb] = get_plotting_CoI(datatype, char(participants(participantnum)), condition, cutoff, iEEG1.times);
 
-%% Plot Co-I, MI and Masks
-a = figure(1);
-tiledlayout(4,3,'TileSpacing','Compact');
-
-%This is the combined synergetic/redundant
-ax(1) = nexttile(5);
-set (gcf,'renderer','Painters','Position', [10 10 800 450]);
-timing = dvt.times;
-contourf(timing,timing,FFi, 50,'linecolor','none');
-hold on
-xlim(xlimits);  ylim(ylimits_CoI);
-set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', clim);
-colormap(ax(1), redblue)
-
-%MI1
-ax(12) = nexttile(2);
-stdshade_acj(FFmi1',.2,'g',timing);
-xlim(xlimits);  ylim(ylimit_MI);
-set(gca,'ytick',yticks_MI, 'yticklabel', y_labels_MI, 'xtick', xticks_CoI, 'xticklabel', x_labels);
-
-%MI2
-ax(14) = nexttile(4);
-stdshade_acj(FFmi2',.2,'g',timing);  view(-90,90)
-xlim(xlimits);  ylim(ylimit_MI);
-set(gca,'ytick',yticks_MI, 'yticklabel', y_labels_MI, 'xtick', xticks_CoI, 'xticklabel', x_labels);
-
-%Combined synergetic/redundant mask
-ax(2) = nexttile(6);
-imagesc(timing,timing,FFm./temp_nb);
-set(gca,'YDir','normal');
-xlim(xlimits);  ylim(ylimits_CoI);
-set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', climits_mask);
-colormap(ax(2), flipud(bone))
-
-%Redundancy CoI
-ax(3) = nexttile(8);
-set (gcf,'renderer','Painters','Position', [10 10 800 450]);
-contourf(timing,timing,FFir, 50,'linecolor','none');
-xlim(xlimits);  ylim(ylimits_CoI);
-set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', climits);
-colormap(ax(3), redblue)
-
-%synergy COI
-ax(4) = nexttile(11);
-set (gcf,'renderer','Painters','Position', [10 10 800 450]);
-contourf(timing,timing,FFis, 50,'linecolor','none');
-xlim(xlimits);  ylim(ylimits_CoI);
-set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', climits);
-colormap(ax(4), redblue)
-
-%Redundant mask
-ax(6) = nexttile(9);
-imagesc(timing,timing,FFmr./temp_nb);
-set(gca,'YDir','normal');
-xlim(xlimits);  ylim(ylimits_CoI);
-set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', climits_mask);
-colormap(ax(6), flipud(bone))
-colorbar;
-
-%Synergy mask
-ax(7) = nexttile(12);
-imagesc(timing,timing,FFms./temp_nb);
-set(gca,'YDir','normal');
-xlim(xlimits);  ylim(ylimits_CoI);
-set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', climits_mask);
-colormap(ax(7), flipud(bone))
-
-ax(1) = nexttile(1);
-set(gca,'ytick',[], 'yticklabel', [], 'xtick',[], 'xticklabel', [], 'clim', clim);
-colorbar; colormap(ax(1), redblue);
-
-saveas(a,'VT_BB_within_local.pdf','pdf')
-saveas(a,'VT_BB_within_local.png','png')
 
