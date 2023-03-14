325fc4b154477219e6d795a2ade029a8c85a951a
Juho Äijälä <57116597+aijala@users.noreply.github.com>
2023-03-14 18:41:23 +0200

    Update plot_highest.m
    Now plots average of highest electrodes of all participants + highest electrodes individually for each participant

--- Plotting/plot_highest.m
+++ Plotting/plot_highest.m
@@ -1,5 +1,5 @@
 %Plots highest temporal electrode
-function [Temp_figure, Front_figure] = plot_highest(basefold, participant, participants, condition, HighAF, times, xlimits, ylimits_CoI, ylimit_MI, xticks_CoI, yticks_CoI, yticks_MI, x_labels, y_labels_CoI, y_labels_MI, climits, climits_mask)
+function [Temp_figure_all, Front_figure_all] = plot_highest(basefold, participant, participants, condition, HighAF, times, xlimits, ylimits_CoI, ylimit_MI, xticks_CoI, yticks_CoI, yticks_MI, x_labels, y_labels_CoI, y_labels_MI, climits, climits_mask)
 
 %%Define matrices for averaged CoI + masks
 tempFFiall = [];
@@ -36,21 +36,21 @@ end
 tempFFi = squeeze(mean(tempFFiall,3));
 tempFFir = tempFFi;
 tempFFis = tempFFi;
-tempFFir(tempFFi <= 0 ) = 0; 
-tempFFis(tempFFi >= 0 ) = 0; 
+tempFFir(tempFFi <= 0 ) = 0;
+tempFFis(tempFFi >= 0 ) = 0;
 
 frontFFi = squeeze(mean(frontFFiall,3));
 frontFFir = frontFFi;
 frontFFis = frontFFi;
-frontFFir(frontFFi <= 0 ) = 0; 
-frontFFis(frontFFi >= 0 ) = 0; 
+frontFFir(frontFFi <= 0 ) = 0;
+frontFFis(frontFFi >= 0 ) = 0;
 
 frontFFmi1 = cat(2,frontFFmi1,frontFFmi1);
 tempFFmi1 = cat(2,tempFFmi1,tempFFmi1);
 
 
 %% PLOT FRONTAL AVERAGE
-Front_figure = figure(6);
+Front_figure_all = figure(6);
 tiledlayout(4,3,'TileSpacing','Compact');
 
 %This is the combined synergetic/redundant
@@ -122,11 +122,11 @@ colorbar; colormap(ax(1), redblue);
 filename = char(strcat(participant,'highest_average_frontal_', condition));
 
 cd (strcat(basefold,'Results\Marmo_EcoG\Figures'))
-saveas(Front_figure,strcat(filename,'.pdf'),'pdf');
-saveas(Front_figure,strcat(filename,'.fig'),'fig');
+saveas(Front_figure_all,strcat(filename,'.pdf'),'pdf');
+saveas(Front_figure_all,strcat(filename,'.fig'),'fig');
 
 %% PLOT TEMPORAL AVERAGE
-Temp_figure = figure(7);
+Temp_figure_all = figure(7);
 tiledlayout(4,3,'TileSpacing','Compact');
 
 %This is the combined synergetic/redundant
@@ -195,10 +195,193 @@ ax(1) = nexttile(1);
 set(gca,'ytick',[], 'yticklabel', [], 'xtick',[], 'xticklabel', [], 'clim', clim);
 colorbar; colormap(ax(1), redblue);
 
-filename = char(strcat(participant,'highest_average_temporal_', condition));
+filename = char(strcat('Highest_average_temporal_', condition));
 
 cd (strcat(basefold,'Results\Marmo_EcoG\Figures'))
-saveas(Temp_figure,strcat(filename,'.pdf'),'pdf');
-saveas(Temp_figure,strcat(filename,'.fig'),'fig');
+saveas(Temp_figure_all,strcat(filename,'.pdf'),'pdf');
+saveas(Temp_figure_all,strcat(filename,'.fig'),'fig');
 
+%% Plot individual electrodes for all participants
+
+for participanti = 1:length(participants)
+
+    %%Define matrices for averaged CoI + masks for individual participants
+    tempFFi = HighAF.(char(participants(participanti))).temp.data;
+    tempFFm = logical(HighAF.(char(participants(participanti))).temp.sigMask);
+    tempFFmr = logical(HighAF.(char(participants(participanti))).temp.sigMask > 0);
+    tempFFms = logical(HighAF.(char(participants(participanti))).temp.sigMask < 0);
+    tempFFmi1 = HighAF.(char(participants(participanti))).temp.MI;
+
+    frontFFi = HighAF.(char(participants(participanti))).front.data;
+    frontFFm = logical(HighAF.(char(participants(participanti))).front.sigMask);
+    frontFFmr = logical(HighAF.(char(participants(participanti))).front.sigMask > 0);
+    frontFFms = logical(HighAF.(char(participants(participanti))).front.sigMask < 0);
+    frontFFmi1 = HighAF.(char(participants(participanti))).front.MI;
+
+    frontFFmi1 = cat(2,frontFFmi1,frontFFmi1);
+    tempFFmi1 = cat(2,tempFFmi1,tempFFmi1);
+    
+    tempFFir = tempFFi;
+    tempFFis = tempFFi;
+    tempFFir(tempFFi <= 0 ) = 0;
+    tempFFis(tempFFi >= 0 ) = 0;
+
+    frontFFir = frontFFi;
+    frontFFis = frontFFi;
+    frontFFir(frontFFi <= 0 ) = 0;
+    frontFFis(frontFFi >= 0 ) = 0;
+
+    %% PLOT FRONTAL AVERAGE
+    Front_figure = figure(6);
+    tiledlayout(4,3,'TileSpacing','Compact');
+
+    %This is the combined synergetic/redundant
+    ax(1) = nexttile(5);
+    set (gcf,'renderer','Painters','Position', [10 10 800 450]);
+    contourf(times,times,frontFFi, 50,'linecolor','none');
+    hold on
+    xlim(xlimits);  ylim(ylimits_CoI);
+    set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', climits);
+    colormap(ax(1), redblue)
+
+    %MI1
+    ax(12) = nexttile(2);
+    stdshade_acj(frontFFmi1',.2,'g',times);
+    xlim(xlimits);  ylim(ylimit_MI);
+    set(gca,'ytick',yticks_MI, 'yticklabel', y_labels_MI, 'xtick', xticks_CoI, 'xticklabel', x_labels);
+
+    %MI2
+    ax(14) = nexttile(4);
+    stdshade_acj(frontFFmi1',.2,'g',times);  view(-90,90)
+    xlim(xlimits);  ylim(ylimit_MI);
+    set(gca,'ytick',yticks_MI, 'yticklabel', y_labels_MI, 'xtick', xticks_CoI, 'xticklabel', x_labels);
+
+    %Combined synergetic/redundant mask
+    ax(2) = nexttile(6);
+    imagesc(times,times,frontFFm./channel_nb);
+    set(gca,'YDir','normal');
+    xlim(xlimits);  ylim(ylimits_CoI);
+    set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', climits_mask);
+    colormap(ax(2), flipud(bone))
+
+    %Redundancy CoI
+    ax(3) = nexttile(8);
+    set (gcf,'renderer','Painters','Position', [10 10 800 450]);
+    contourf(times,times,frontFFir, 50,'linecolor','none');
+    xlim(xlimits);  ylim(ylimits_CoI);
+    set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', climits);
+    colormap(ax(3), redblue)
+
+    %synergy COI
+    ax(4) = nexttile(11);
+    set (gcf,'renderer','Painters','Position', [10 10 800 450]);
+    contourf(times,times,frontFFis, 50,'linecolor','none');
+    xlim(xlimits);  ylim(ylimits_CoI);
+    set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', climits);
+    colormap(ax(4), redblue)
+
+    %Redundant mask
+    ax(6) = nexttile(9);
+    imagesc(times,times,frontFFmr./channel_nb);
+    set(gca,'YDir','normal');
+    xlim(xlimits);  ylim(ylimits_CoI);
+    set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', climits_mask);
+    colormap(ax(6), flipud(bone))
+    colorbar;
+
+    %Synergy mask
+    ax(7) = nexttile(12);
+    imagesc(times,times,frontFFms./channel_nb);
+    set(gca,'YDir','normal');
+    xlim(xlimits);  ylim(ylimits_CoI);
+    set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', climits_mask);
+    colormap(ax(7), flipud(bone))
+
+    ax(1) = nexttile(1);
+    set(gca,'ytick',[], 'yticklabel', [], 'xtick',[], 'xticklabel', [], 'clim', clim);
+    colorbar; colormap(ax(1), redblue);
+
+    filename = char(strcat(participant,'highest_average_frontal_', condition));
+
+    cd (strcat(basefold,'Results\Marmo_EcoG\Figures'))
+    saveas(Front_figure,strcat(filename,'.pdf'),'pdf');
+    saveas(Front_figure,strcat(filename,'.fig'),'fig');
+
+    %% PLOT TEMPORAL AVERAGE
+    Temp_figure = figure(7);
+    tiledlayout(4,3,'TileSpacing','Compact');
+
+    %This is the combined synergetic/redundant
+    ax(1) = nexttile(5);
+    set (gcf,'renderer','Painters','Position', [10 10 800 450]);
+    contourf(times,times,tempFFi, 50,'linecolor','none');
+    hold on
+    xlim(xlimits);  ylim(ylimits_CoI);
+    set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', climits);
+    colormap(ax(1), redblue)
+
+    %MI1
+    ax(12) = nexttile(2);
+    stdshade_acj(tempFFmi1',.2,'g',times);
+    xlim(xlimits);  ylim(ylimit_MI);
+    set(gca,'ytick',yticks_MI, 'yticklabel', y_labels_MI, 'xtick', xticks_CoI, 'xticklabel', x_labels);
+
+    %MI2
+    ax(14) = nexttile(4);
+    stdshade_acj(tempFFmi1',.2,'g',times);  view(-90,90)
+    xlim(xlimits);  ylim(ylimit_MI);
+    set(gca,'ytick',yticks_MI, 'yticklabel', y_labels_MI, 'xtick', xticks_CoI, 'xticklabel', x_labels);
+
+    %Combined synergetic/redundant mask
+    ax(2) = nexttile(6);
+    imagesc(times,times,tempFFm./channel_nb);
+    set(gca,'YDir','normal');
+    xlim(xlimits);  ylim(ylimits_CoI);
+    set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', climits_mask);
+    colormap(ax(2), flipud(bone))
+
+    %Redundancy CoI
+    ax(3) = nexttile(8);
+    set (gcf,'renderer','Painters','Position', [10 10 800 450]);
+    contourf(times,times,tempFFir, 50,'linecolor','none');
+    xlim(xlimits);  ylim(ylimits_CoI);
+    set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', climits);
+    colormap(ax(3), redblue)
+
+    %synergy COI
+    ax(4) = nexttile(11);
+    set (gcf,'renderer','Painters','Position', [10 10 800 450]);
+    contourf(times,times,tempFFis, 50,'linecolor','none');
+    xlim(xlimits);  ylim(ylimits_CoI);
+    set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', climits);
+    colormap(ax(4), redblue)
+
+    %Redundant mask
+    ax(6) = nexttile(9);
+    imagesc(times,times,tempFFmr./channel_nb);
+    set(gca,'YDir','normal');
+    xlim(xlimits);  ylim(ylimits_CoI);
+    set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', climits_mask);
+    colormap(ax(6), flipud(bone))
+    colorbar;
+
+    %Synergy mask
+    ax(7) = nexttile(12);
+    imagesc(times,times,tempFFms./channel_nb);
+    set(gca,'YDir','normal');
+    xlim(xlimits);  ylim(ylimits_CoI);
+    set(gca,'ytick',yticks_CoI, 'yticklabel', y_labels_CoI, 'xtick',xticks_CoI, 'xticklabel', x_labels, 'clim', climits_mask);
+    colormap(ax(7), flipud(bone))
+
+    ax(1) = nexttile(1);
+    set(gca,'ytick',[], 'yticklabel', [], 'xtick',[], 'xticklabel', [], 'clim', clim);
+    colorbar; colormap(ax(1), redblue);
+
+    filename = char(strcat(participant,'highest_temporal_', condition));
+
+    cd (strcat(basefold,'Results\Marmo_EcoG\Figures'))
+    saveas(Temp_figure,strcat(filename,'.pdf'),'pdf');
+    saveas(Temp_figure,strcat(filename,'.fig'),'fig');
+
+end
 end
