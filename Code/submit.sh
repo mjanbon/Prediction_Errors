2f1613c1405a9d57189be7eabfb60d99c1f64c5b
maxime_janbon <maxime.janbon1@outlook.com>
2024-09-13 17:10:23 +0100

    Next Co-Is

--- Code/submit.sh
+++ Code/submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -p icelake
 #SBATCH -t 00:30:00
-#SBATCH --array=1-2
+#SBATCH --array=3-4
 #SBATCH -J Drosophila_Sleep_CoI_R22
 #SBATCH -o logs/Drosophila_Sleep_CoI_R22_%A_%a.out
 #SBATCH -e logs/Drosophila_Sleep_CoI_R22_%A_%a.err
