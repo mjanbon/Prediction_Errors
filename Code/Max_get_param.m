fefe9714bf0f30dce8c7085cce8d0b947f438379
maxime_janbon <maxime.janbon1@outlook.com>
2024-11-25 13:42:31 +0000

    Correct fly

--- Code/Max_get_param.m
+++ Code/Max_get_param.m
@@ -45,7 +45,7 @@ subject   = 1 ;
 %DROSOPHILA
 % participants = {'R230720','R040820_1','R040820_2', 'R050820_1'...
 %     , 'R060820_1', 'R060820_2', 'R060721'};
-participants = {'R230720_1'};
+participants = {'R230720'};
 
 %MARMOSETS
 % participants = {'Ji' 'Nr'}
