cb0f89bd931848718b5a99a92c52102f90fce529
maxime_janbon <maxime.janbon1@outlook.com>
2026-02-08 16:51:46 +0000

    All drosophila CoI comparison

--- Code/apocrita_submit_comparison_CoI.sh
+++ Code/apocrita_submit_comparison_CoI.sh
@@ -6,7 +6,7 @@
 #$ -o /data/SBBS-PIDProject/Maxime/Logs/CoI_Comparison/
 #$ -e /data/SBBS-PIDProject/Maxime/Logs/CoI_Comparison/
 #$ -l h_rt=239:00:00
-#$ -t 1-2
+#$ -t 3-1800
 
 
 # Load MATLAB ready for processing.
