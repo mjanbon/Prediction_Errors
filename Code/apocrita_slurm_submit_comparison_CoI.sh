cea03e39999426280e1c5bfb774d4fc97b1c9169
maxime_janbon <maxime.janbon1@outlook.com>
2026-04-06 14:46:50 +0100

    Extend beginning sleep comparison to CoI

---
+++ Code/apocrita_slurm_submit_comparison_CoI.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -o /data/SBBS-PIDProject/Maxime/Logs/CoI_Comparison/CoI_permute_comparison_%j.log
+#SBATCH --mem-per-cpu=16G    # Request 16G per cpu
+#SBATCH -t 239:00:00         # Request 239 hours runtime
+#SBATCH -n 1                # Request 1 core
+#SBATCH -a 1-2               # Array Job-ID 1-2
+#SBATCH --partition=compute # Request the compute partition
+
+
+# Load MATLAB ready for processing.
+module load matlab
+
+
+matlab -nodisplay -nosplash -r "max_main_comparison(${SGE_TASK_ID}); quit"
