90caeaa072f32df5d2c24773a90128f311250e36
maxime_janbon <maxime.janbon1@outlook.com>
2026-04-06 14:26:13 +0100

    Beginning sleep comparison and slurm scripts

--- Code/apocrita_slurm_submit_MI_comparison.sh
+++ Code/apocrita_slurm_submit_MI_comparison.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-#$ -cwd
-#$ -pe smp 1
-#$ -j y
-#$ -l h_vmem=64G
-#$ -o /data/SBBS-PIDProject/Maxime/Logs/Drosophila_MI/
-#$ -e /data/SBBS-PIDProject/Maxime/Logs/Drosophila_MI/
-#$ -l h_rt=239:0:0
-#$ -l rocky
-
+#SBATCH -o /data/SBBS-PIDProject/Maxime/Logs/Drosophila_MI/MI_permute_comparison_%j.log
+#SBATCH --mem-per-cpu=64G    # Request 64G per cpu
+#SBATCH -t 239:00:00         # Request 239 hours runtime
+#SBATCH -n 1                # Request 1 core
+#SBATCH --partition=compute # Request the compute partition
 
 # Load MATLAB ready for processing.
 module load matlab
