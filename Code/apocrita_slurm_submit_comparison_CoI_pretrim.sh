cfb9a6908317a37f352cc9d58019460fcf07ebad
maxime_janbon <maxime.janbon1@outlook.com>
2026-06-03 10:13:05 +0100

    Remaining flies wake sleep CoI pretrim

--- Code/apocrita_slurm_submit_comparison_CoI_pretrim.sh
+++ Code/apocrita_slurm_submit_comparison_CoI_pretrim.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem-per-cpu=16G    # Request 16G per cpu
 #SBATCH -t 239:00:00         # Request 239 hours runtime
 #SBATCH -n 1                # Request 1 core
-#SBATCH -a 1-225               # Array Job-ID 1-1800 but start with first fly
+#SBATCH -a 226-1800               # Array Job-ID 1-1800 do all other flies
 #SBATCH --partition=compute # Request the compute partition
 
 
