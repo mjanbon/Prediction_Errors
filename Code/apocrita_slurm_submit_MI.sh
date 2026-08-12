841a0920816f3f30f138969fbdc67f295b011369
maxime_janbon <maxime.janbon1@outlook.com>
2026-08-12 14:44:16 +0100

    Running mid-sleep on its own

---
+++ Code/apocrita_slurm_submit_MI.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -o /data/SBBS-PIDProject/Maxime/Logs/Drosophila_MI/MI_mid_sleep_%j.log
+#SBATCH --mem-per-cpu=64G    # Request 64G per cpu
+#SBATCH -t 239:00:00         # Request 239 hours runtime
+#SBATCH -n 1                # Request 1 core
+#SBATCH --partition=compute # Request the compute partition
+
+# Load MATLAB ready for processing.
+module load matlab
+
+matlab -nodisplay -nosplash -r "main_MI_ERP(1); quit"
--- Code/main_MI_ERP.m
+++ Code/main_MI_ERP.m
@@ -165,13 +165,13 @@ for i = 1: length(participants)
             c_tag='C';
         end
         cd('MI_Figures') % Save MI figures for the fly
-
-        saveas(h,strcat(char(participants(i)),'_', char(activity_tag), '_',char(all_con(con)),...
+        set(h, 'Renderer', 'painters');
+        print(h, strcat(char(participants(i)),'_', char(activity_tag), '_',char(all_con(con)),...
             c_tag, num2str(standard_group_number),...
-            num2str(deviant_group_number),'_MI_figures_local'),'fig');
+            num2str(deviant_group_number),'_MI_figures_local'), '-dsvg');
         saveas(H,strcat(char(participants(i)),'_', char(activity_tag), '_', char(all_con(con)),...
             c_tag, num2str(standard_group_number),...
-            num2str(deviant_group_number),'_ERPs_local'),'fig');
+            num2str(deviant_group_number),'_ERPs_local'),'svg');
         cd ../
         cd MI_Data
         MI_name = char(strcat(participants(i),char(activity_tag),char(all_con(con)),'_MI_data.mat'));
